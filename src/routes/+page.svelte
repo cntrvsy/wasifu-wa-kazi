a7c07ee149e8c20d7f8d9b7f2a97cfe523b0cca5
cntrvsy <benintangana@gmail.com>
2024-09-03 01:34:59 +0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  // import Sun from "lucide-svelte";
+  import { toggleMode } from "mode-watcher";
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { invoke } from "@tauri-apps/api/core";
+
+  let name = "";
+  let greetMsg = "";
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name });
+  }
+</script>
+<div class="">
+  <form class="row" on:submit|preventDefault={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+
+  <p>{greetMsg}</p>
+
+  <Button on:click={toggleMode}>
+    <span class="sr-only">Toggle theme</span>
+  </Button>
+
+  <!-- <Sun strokeWidth={1.5} size ={72} /> -->
+</div>
+ 
